4eaa9590fe8b141b2697fce7909a973337008aba
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-03 00:05:38 +0200

    Including FractionTest Class and UserTest Class in project.

--- src/test/java/es/upm/miw/swc/FractionTest.java
+++ src/test/java/es/upm/miw/swc/FractionTest.java
@@ -5,6 +5,7 @@ package es.upm.miw.swc;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,10 +13,39 @@ import org.junit.Test;
  *
  */
 public class FractionTest {
+	private Fraction fraction;
 
+	@Before
+	public void before() {
+	    fraction = new Fraction(7, 9);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFractionIntInt() {
+	    assertEquals(7, fraction.getNumerator());
+	    assertEquals(9, fraction.getDenominator());
+	}
+	
+	@Test
+	public void testFraction() {
+	    fraction = new Fraction();
+	    assertEquals(1, fraction.getNumerator());
+	    assertEquals(1, fraction.getDenominator());
+	}
+	
+	@Test
+	public void testGetNumerator() {
+	    assertEquals(7, fraction.getNumerator());
+	}
+	
+	@Test
+	public void testGetDenominator() {
+	    assertEquals(9, fraction.getDenominator());
+	}
+	
+	@Test
+	public void testDecimal() {
+	    assertEquals((double)7/9, fraction.decimal(), 0.0001);
 	}
 
 }
