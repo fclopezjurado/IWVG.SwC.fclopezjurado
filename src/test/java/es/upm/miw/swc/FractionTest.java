73e07687072ea9a19be3b165825bb85abdf3b746
fclopezjurado <fclopezjurado@gmail.com>
2016-10-02 22:09:01 +0200

    Installing SWC Project

---
+++ src/test/java/es/upm/miw/swc/FractionTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package es.upm.miw.swc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author franlopez
+ *
+ */
+public class FractionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
